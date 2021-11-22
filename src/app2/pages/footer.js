0f64059be7fbe323e2ec583a07c55a9660e80c62
tatekitzhak <tatekosi@gmail.com>
2021-11-22 22:14:46 +0200

    Added a new files.

---
+++ src/app2/pages/footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+import Footer from '../js/components/footer';
+
+function Foooter(props) {
+    return(
+    <>
+        <h1>Foooter</h1>
+    </>);
+};
+
+export default Footer;
---
+++ src/app2/pages/sidebar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+function SideBar(props) {
+    return(
+    <>
+        <h1>SideBar</h1>
+    </>);
+};
+
+export default SideBar; 
