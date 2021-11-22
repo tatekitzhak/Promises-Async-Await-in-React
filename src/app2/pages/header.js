422940584ae59736bc79df2c37ba196316c9ff5e
tatekitzhak <tatekosi@gmail.com>
2021-11-22 22:11:50 +0200

    Added a new files.

---
+++ src/app2/pages/header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+function Header(props) {
+    return(
+    <>
+        <h1>Header</h1>
+    </>);
+};
+
+export default Header;
+ 
---
+++ src/app2/pages/main.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+
+function Main(props) {
+    return(
+    <>
+        <h1>Main</h1>
+    </>);
+};
+
+export default Main;
