08abf3bd573878d94822d25c971810a6ef01ad19
tatekitzhak <tatekosi@gmail.com>
2021-11-27 21:00:26 +0200

    Modify an existing files.

--- src/app3/ui-components/items-results/element.js
+++ src/app3/ui-components/items-results/element.js
@@ -12,11 +12,23 @@ function Element(props) {
         <>
             <div>
                 <section>
+                    <div class="link-nodes-path">
+                        <ul class="links breadcrumb">
+                            <li class="link-node"><a href="#" >Path: </a></li>
+                            <li class="link-node"><a href="#">A/</a></li>
+                            <li class="link-node"><a href="#">B/</a></li>
+                            <li class="link-node"><a href="#">C/</a></li>
+                            <li class="link-node"><a href="#">D/</a></li>
+                            <li class="link-node"><a href="#">E</a></li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
                     <div className="services-grid">
                         <div className="service service1">
                             <h4>Convert Audio File To Text</h4>
                             <p className="section-lead">This workflow shows the concept functionality of using the online to text converter.</p>
-                           
+
                         </div>
                     </div>
                     <div class="surveyContainer" id="survey-anchor">
