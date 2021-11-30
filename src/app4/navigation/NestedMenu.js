13654094584ba2fe853fa46fb8b84adc7fb4be8b
tatekitzhak <tatekosi@gmail.com>
2021-11-30 15:50:00 +0200

    Modify an existing files.

--- src/app4/navigation/NestedMenu.js
+++ src/app4/navigation/NestedMenu.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { pathTo } from "./utils";
+
+
 function Menu({ routes, level }) {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (level == 1) {
+      setData(level);
+    }
+
+  }, [level, setData]);
   return (
     <>
-      <nav className={`menu menu-level-${level}`}>
-        {
-          routes.map((route, index) => (
-          <NavLink key={index} to={route.path}>
-            {route.label}
-          </NavLink>
-        ))
-        }
+      <nav className={`menu menu-level-${level} `}>
+        {console.log('Menu:', data)}
+        <ul>
+          {
+            routes.map((route, index) => (
+              <li>
+                <NavLink key={index} to={route.path}>
+                  {route.label}
+                </NavLink>
+              </li>
+            ))
+          }
+        </ul>
+
       </nav>
     </>
   );
@@ -20,13 +36,15 @@ function Menu({ routes, level }) {
 function NestedMenu({ route }) {
   return (
     <>
-      {
-        pathTo(route)
-        .filter(r => r.routes)
-        .map((r, index) => (
-          <Menu key={index} routes={r.routes} level={index} />
-        ))
+      <div className="drawer drawer--left">
+        {
+          pathTo(route)
+            .filter(r => r.routes)
+            .map((r, index) => (
+              <Menu key={index} routes={r.routes} level={index} />
+            ))
         }
+      </div>
     </>
   );
 }
