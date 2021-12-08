a7b26438d48413c46643e0b54630fdf99bf0afef
tatekitzhak <tatekosi@gmail.com>
2021-12-08 23:05:14 +0200

    Modify an existing file.

--- src/app4/navigation/NestedMenu.js
+++ src/app4/navigation/NestedMenu.js
@@ -8,29 +8,43 @@ function NestedMenu(props) {
   const route = props.route;
   const typeId = props.pid;
 
-  useEffect(() => {
-    // console.log('NestedMenu1:', route.routes, typeId)
-    // console.log('NestedMenu2:', route.homePage, typeId)
-    // console.log('NestedMenu3:', route.footer, typeId)
-    if (typeId == 'pageId' && route.routes) {
-      console.log('pageId NestedMenu:', route.routes)
-    } else if (typeId == 'homeId' && route.homePage) {
-      console.log('homeId NestedMenu:', route.homePage)
-    } else if (typeId == 'footerId' && route.footer) {
-      console.log('footerId NestedMenu:', route.footer)
-    }
-  }, [route])
+  /* if (typeId == 'pageId' && route.routes) {
+    console.log('pageId NestedMenu:', route.routes)
+  } else if (typeId == 'homeId' && route.homePage) {
+    console.log('homeId NestedMenu:', route.homePage)
+  } else if (typeId == 'footerId' && route.footer) {
+    console.log('footerId NestedMenu:', route.footer)
+  } */
 
+  function toggleReducer(action) {
+
+    console.log('toggleReducer:', action.rt.homePage)
+    switch (action.typeId) {
+      case typeId: {
+         console.log('pageId NestedMenu:', route.routes)
+      }
+      case typeId: {
+        return (<Menu key={action.index} homePage={action.rt.homePage} level={action.index} {...props}>
+                  <h1>Menu</h1>
+                </Menu>)
+      }
+      case typeId: {
+        console.log('footerId NestedMenu:', route.footer)
+      }
+      default: {
+        throw new Error(`Unhandled type:`)
+      }
+    }
+  }
 
   return (
     <>
-
       {
         (typeId == 'pageId') ? (pathTo(route)
                                 .filter(rt => rt.routes)
                                 .map(function (rt, index) {
                                   { console.log('rt:', index, rt) }
-                                  if (index == 1 && rt.id == "task1") {
+                                  if (index == 1 && rt.id == "topics") {
                                     return (
                                       <div key={index} className="drawer">
                                         <button type="button" className="drawer-toggle drawer-hamburger">
@@ -52,10 +66,10 @@ function NestedMenu(props) {
           (typeId == 'homeId') ? (pathTo(route)
                                   .filter(rt => rt.homePage)
                                   .map(function (rt, index) {
-                                    return (
-                                      <Menu key={index} homePage={rt.homePage} level={index} {...props}>
-                                        <h1>Menu</h1>
-                                      </Menu>
+                                    return ( toggleReducer({typeId, index, rt})
+                                      // <Menu key={index} homePage={rt.homePage} level={index} {...props}>
+                                      //   <h1>Menu</h1>
+                                      // </Menu>
                                     )
                                   })) :
           (typeId == 'footerId') ? (pathTo(route)
